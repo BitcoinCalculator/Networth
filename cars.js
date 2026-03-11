e2439fe858d6b8ecb9ae7b40a53ee136820b7c83
BitcoinCalculator <marknjoyce23@gmail.com>
2026-03-10 21:49:49 -0700

    Add renderCars function for car management

---
+++ cars.js
@@ -0,0 +1,7 @@
+// Similar to workers.js
+async function renderCars() {
+  // List cars, add/edit/delete
+  // For each car, show maintenance list/log
+}
+
+// Maintenance add/complete logs updates mileage if applicable
